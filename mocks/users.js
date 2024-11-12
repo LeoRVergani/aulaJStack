a3fbf1518c407dd8748ee7e57f18eae94e2eb234
leorvergani <vergani@outlook.com.br>
2024-11-12 00:12:22 -0300

    nodejs jstack

---
+++ mocks/users.js
@@ -0,0 +1,18 @@
+module.exports = [
+    {
+        id: 1,
+        name: 'Leonardo',
+    },
+    {
+        id: 2,
+        name: 'João',
+    },
+    {
+        id: 3,
+        name: 'Pablo',
+    },
+    {
+        id: 4,
+        name: 'Roberto',
+    },
+];
